bf247e68cd91221c3eff631c1ad86efeea5f00d6
sharv-d021 <sharvaridgaikwad345@gmail.com>
2025-09-05 06:44:01 +0000

    conversion of datatypes

---
+++ 01_basic/conversionOperation.js
@@ -0,0 +1,33 @@
+/*
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+*/
+
+//"33" => 33
+//"33abc" => NaN(Not a Number)
+// true => 1; false => 0
+
+let isLoggedIn = "Sharv"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Sharv" => true
+
+let num = 33
+
+let string = String(num)
+console.log(string);
+console.log(typeof string);
+
+
+
+
