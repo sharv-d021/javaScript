6a97b2c332d98666e5ed6cfa255fb2482b950d12
sharv-d021 <sharvaridgaikwad345@gmail.com>
2025-09-05 06:10:35 +0000

    datatypes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //This is used to apply newer version of js
+
+// alert(3+3) // we are using node js and not browser, it will run on browser.
+
+let name = "Sharv"
+let age = 20
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint => for bigg number large number
+// string => Can use in "" or ''
+// boolean => true or false
+// null =>  it is stand alone value, representation an empty value
+// undefined => value assign na hona
+// symbol => uniqueness
+
+// object
+
+console.log(typeof "sharv");        //string
+console.log(typeof age);            //int
+console.log(typeof undefined);      // undefine
+console.log(typeof null);           // object
+
+
+
